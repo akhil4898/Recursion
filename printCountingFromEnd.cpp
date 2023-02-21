12412b3e050abce66b8629eb8d980332dbd95789
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2023-02-21 12:20:53 +0530

    Update file

---
+++ printCountingFromEnd.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void Counting(int n)
+{
+    // Base case...
+    if(n == 0)
+    {
+        return;
+    }
+    // Proceesing...
+    cout << n << endl;
+    // Recursive Relation...
+    Counting(n-1);
+}
+
+
+int main()
+{
+
+    int n;
+    cout << "Enter num: ";
+    cin >> n;
+    Counting(n);
+    return 0;
+}
