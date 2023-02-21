f472f175419beefc8b4ecedd24f9675bf6427b70
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2023-02-21 16:59:51 +0530

    Update file

---
+++ sayDigit..cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+
+void sayDigit(int n, string arr[])
+{
+    // Base case..
+    if( n == 0)
+        return;
+
+    // Processing 
+    
+    int digit = n % 10;
+    n = n / 10;
+
+
+    // Recursive Relation..
+    sayDigit(n, arr);
+
+    cout << arr[digit] << " ";
+
+}
+
+
+int main()
+{
+    string arr[10] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    int n;
+    cout << "Enter num: ";
+    cin >> n;
+    cout << "Your Digit is: ";
+    sayDigit(n, arr);
+    cout << endl;
+    return 0;
+}
