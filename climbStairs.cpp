0b3045ef1f4ddf5efeff9f40b63f10e291a2599a
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2023-02-21 16:47:43 +0530

    Update file

---
+++ climbStairs.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int climbStairs(int n)
+{
+    // Base case..
+
+    if(n < 0)
+        return 0;
+    
+    if(n == 0)
+        return 1;
+
+    // Recursive relation..
+
+    return climbStairs(n-1) + climbStairs(n-2);
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter num: ";
+    cin >> n;
+    cout << "Ways to reach nth stairs: " << climbStairs(n) << endl;
+    return 0;
+}
