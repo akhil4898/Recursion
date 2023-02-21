e1eaab7e343b862f88ff26a56a338382a9d85830
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2023-02-21 13:16:38 +0530

    Update file

---
+++ fibonnacciSeries.cpp
@@ -0,0 +1,34 @@
+#include "iostream"
+
+using namespace std;
+
+int Fib(int n)
+{
+    // Base case...
+    if(n == 0 || n == 1)
+        return n;
+
+    // Recursive Relation...
+    else
+        return Fib(n-1) + Fib(n-2);
+    cout << n;
+}
+
+
+
+int main()
+{
+
+    int num;
+
+    cout << "Enter num: ";
+    cin >> num;
+
+    cout << "Fibonnacci Series is: ";
+    for (int i = 0; i < num; i++)
+    {
+        cout << " " << Fib(i);
+    }
+    cout << endl;
+    return 0;
+}
